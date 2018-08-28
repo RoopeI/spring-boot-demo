893356847dfd5f8ed485ad663f511c5734a200d5
roopei <roope.ikonen@phz.fi>
2018-08-28 09:37:07 +0300

    Logging added

--- Readme.md
+++ Readme.md
@@ -3,9 +3,8 @@
 dockerfile-maven-plugin is used to build the app itself and the docker image
 - mvn install dockerfile:build
 
-
 # Run docker
-- docker run -itd -p 5000:8080 <image-id>
+- docker run -itd -p 5000:8080 -v /<path>/logs:/logs ropasoft/spring-boot-demo
 
 # Log in
 - docker exec -it <container-id> sh
--- src/main/java/fi/ropasoft/bootdemo/RootController.java
+++ src/main/java/fi/ropasoft/bootdemo/RootController.java
@@ -1,13 +1,17 @@
 package fi.ropasoft.bootdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
+    private Logger logger = LoggerFactory.getLogger(RootController.class);
 
     @RequestMapping("/")
     public String index(){
+        logger.debug("index() - start");
         return "{'name': 'index'}";
     }
 }
